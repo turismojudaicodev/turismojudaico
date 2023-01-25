a864edcf42758c1f782e22b09e4ef8cae09d3bf4
Lucas <lucaspiputto@gmail.com>
2023-01-25 16:56:43 -0300

    Add next/font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Open_Sans } from '@next/font/google'
+
+const openSans = Open_Sans({
+  weight: ['400', '600'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={openSans.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
