38798d6f1c485be0695daaafd3c5aacc864f5606
Lucas <lucaspiputto@gmail.com>
2023-01-27 13:56:08 -0300

    Add content page

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -4,39 +4,43 @@ import styles from './NavMenu.module.css'
 const LINKS = [
   {
     title: 'quienes somos',
-    url: '/about'
-  },
-  {
-    title: 'blogs',
-    url: 'blogs'
-  },
-  {
-    title: 'audiogías',
-    url: 'audioguides'
+    url: '/about',
   },
   {
     title: 'city tours judios',
-    url: 'citytours'
+    url: 'citytours',
+  },
+  {
+    title: 'audiogías',
+    url: 'audioguides',
+  },
+  {
+    title: 'blogs',
+    url: 'blogs',
+  },
+  {
+    title: 'atracciones judaicas',
+    url: 'content',
   },
   {
     title: 'contacto',
-    url: 'contact'
+    url: 'contact',
   },
   {
     title: 'newsletter',
-    url: 'newsletter'
-  }
+    url: 'newsletter',
+  },
 ]
 
 export default function NavMenu({ active }) {
   return (
     <nav className={active ? styles.navActive : styles.navInactive}>
       <ul className={styles.navLinks}>
-        {LINKS.map(link =>
+        {LINKS.map((link) => (
           <li key={link.url}>
             <Link href={link.url}>{link.title}</Link>
           </li>
-        )}
+        ))}
       </ul>
     </nav>
   )
---
+++ src/pages/content.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+
+export default function Content() {
+  return (
+    <>
+      <Head>
+        <title>Contenido</title>
+      </Head>
+      <Layout>
+        <main>Contenido</main>
+      </Layout>
+    </>
+  )
+}
