5904b156f0063ffbc4abd568ecf4688509bfc816
Lucas <lucaspiputto@gmail.com>
2023-01-27 13:57:10 -0300

    Create test database

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,7 +1,19 @@
-import Layout from '@/components/Layout'
 import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '@/components/Layout'
 
-export default function Blogs() {
+export async function getServerSideProps() {
+  const res = await import('../../db.json')
+  const data = JSON.parse(JSON.stringify(res))
+  const { blogs } = data
+  return {
+    props: {
+      blogs,
+    },
+  }
+}
+
+export default function Blogs({ blogs }) {
   return (
     <>
       <Head>
@@ -9,7 +21,15 @@ export default function Blogs() {
       </Head>
       <Layout>
         <main>
-          blogs
+          {blogs.map((blog) => (
+            <div key={blog.id}>
+              <Image src={blog.img} height={50} width={200} alt="img" />
+              <div>
+                <h3>{blog.title}</h3>
+                <p>{blog.content}</p>
+              </div>
+            </div>
+          ))}
         </main>
       </Layout>
     </>
