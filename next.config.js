c880694748336b1fa7a69acb8b77bd64f55bda2b
Lucas <lucaspiputto@gmail.com>
2023-02-02 20:58:13 -0300

    Delete admin panel page
    Instead redirect to an external admin panel page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
     loader: 'default',
     domains: ['localhost'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://localhost:1337/admin'
+            : process.env.ADMIN_PANEL_URL,
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
