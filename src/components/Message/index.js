6fe001db70d333f21e6e772fc289c6849da45425
Lucas <lucaspiputto@gmail.com>
2023-02-14 15:07:08 -0300

    Change message icons

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,16 +1,24 @@
 import styles from './Message.module.css'
-import infoCircleIcon from 'public/icons/info-circle.svg'
+import infoIcon from 'public/icons/info.svg'
+import errorIcon from 'public/icons/error.svg'
+import warningIcon from 'public/icons/warning.svg'
 import Image from 'next/image'
 
 export default function Message({ type, message }) {
   if (type !== 'warning' && type !== 'error' && type !== 'info')
     throw new Error('Incorrect message type')
 
+  const icon = {
+    info: infoIcon,
+    warning: warningIcon,
+    error: errorIcon,
+  }
+
   return (
     <div className={`${styles[type]}`}>
       <p className={styles.message}>
         <Image
-          src={infoCircleIcon}
+          src={icon[type]}
           width={25}
           height={25}
           className={styles.icon}
