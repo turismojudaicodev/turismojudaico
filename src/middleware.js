d5b6964bc9bb164b4c1a914bc316c1ea6813e5a3
Lucas <lucaspiputto@gmail.com>
2023-04-19 15:58:43 -0300

    Remove middleware unnecessary redirect

--- src/middleware.js
+++ src/middleware.js
@@ -3,11 +3,17 @@ import { NextResponse } from 'next/server'
 export function middleware(req) {
   const user = req.cookies.get('user')?.value
 
-  if (!user || JSON.parse(user).role !== 'admin') {
-    return NextResponse.redirect(new URL('/admin', req.url))
+  if (req.nextUrl.pathname.startsWith('/admin/dashboard')) {
+    if (!user || JSON.parse(user).role !== 'admin') {
+      return NextResponse.redirect(new URL('/admin', req.url))
+    }
+  } else if (req.nextUrl.pathname.startsWith('/admin')) {
+    if (user && JSON.parse(user).role === 'admin') {
+      return NextResponse.redirect(new URL('/admin/dashboard', req.url))
+    }
   }
 }
 
 export const config = {
-  matcher: '/admin/dashboard/:path*',
+  matcher: '/admin/:path*',
 }
