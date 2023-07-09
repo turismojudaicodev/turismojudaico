65c62bf6d9953c2ed6681828b386a55b5e61feda
Lucas <lucaspiputto@gmail.com>
2023-07-09 17:58:46 -0300

    Use same domain for i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,16 +2,6 @@ module.exports = {
   i18n: {
     locales: ['es', 'en'],
     defaultLocale: 'es',
-    domains: [
-      {
-        domain: 'turismojudaico.vercel.app',
-        defaultLocale: 'es',
-      },
-      {
-        domain: 'en.turismojudaico.vercel.app',
-        defaultLocale: 'en',
-      },
-    ],
     localeDetection: false,
   },
   react: { useSuspense: false },
