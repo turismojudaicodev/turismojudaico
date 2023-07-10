fc83f0d3c44d813defe1a52048647c84047f5f08
Lucas <lucaspiputto@gmail.com>
2023-07-09 21:24:15 -0300

    Add audioguides iframe

--- src/pages/audioguides.js
+++ src/pages/audioguides.js
@@ -1,20 +1,12 @@
 // NPM
-import { useEffect, useState } from 'react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
-import { useRouter } from 'next/router'
-// Local
-import { handleError } from 'lib/errors'
 // Components
 import Layout from '@/components/Layout'
-import Message from '@/components/Message'
 import Head from 'next/head'
-import Link from 'next/link'
-import LoadingIndicator from '@/components/LoadingIndicator'
 // Styles
 import styles from '@/styles/Audioguides.module.css'
 import utils from '@/styles/utils.module.css'
-import StrapiImage from '@/components/StrapiImage'
 
 export async function getStaticProps({ locale }) {
   return {
@@ -25,10 +17,6 @@ export async function getStaticProps({ locale }) {
 }
 
 export default function Audioguides() {
-  const [audioguides, setAudioguides] = useState([])
-  const [errorMessage, setErrorMessage] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-
   const { t } = useTranslation('audioguides')
 
   return (
@@ -39,36 +27,17 @@ export default function Audioguides() {
       <Layout>
         <main className={`${utils.container} ${styles.main}`}>
           <h1 className={utils.bigTitle}>{t('body.title')}</h1>
-          {errorMessage ? (
-            <Message type="error" message={errorMessage} />
-          ) : (audioguides.length === 0 && isLoading) || isLoading ? (
-            <LoadingIndicator />
-          ) : audioguides.length > 0 ? (
-            <div>
-              {audioguides.map((audioguide) => {
-                return (
-                  <div key={audioguide.id} className={styles.audioguideCard}>
-                    <div className={styles.imageContainer}>
-                      <StrapiImage />
-                    </div>
-                    <div className={styles.audioguideData}>
-                      <h2>{audioguide.attributes.title}</h2>
-                      <p>{audioguide.attributes.description}</p>
-                      <a
-                        className={utils.button}
-                        href="/images/logo.png"
-                        download={audioguide.attributes.title}
-                      >
-                        Descargar
-                      </a>
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
-          ) : (
-            <Message type="info" message={t('body.alerts.noContent')} />
-          )}
+          <iframe
+            src="https://turismo-judaico.web.app?lng=es"
+            style={{
+              height: '500px',
+              width: '100%',
+              border: '2px solid #ccc',
+              borderRadius: '.5rem',
+              padding: '1rem',
+            }}
+            title="Audioguias"
+          ></iframe>
         </main>
       </Layout>
     </>
