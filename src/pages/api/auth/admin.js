ce4d474e9000d31b5bf0fc3080fce18b7012ea62
Lucas <lucaspiputto@gmail.com>
2023-09-23 10:54:45 -0300

    Add admin users

--- src/pages/api/auth/admin.js
+++ src/pages/api/auth/admin.js
@@ -1,27 +1,37 @@
-import bcrypt from 'bcrypt'
 import { setCookie } from 'cookies-next'
-// import { bd } from 'lib/mysql'
+import { db } from 'lib/mysql'
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { username, password } = req.body
 
-    // const admin = await prisma.user.findUnique({
-    //   where: { username },
-    // })
-    // const isPasswordValid = await bcrypt.compare(password, admin.passwordHash)
-    // if (!admin || admin.role.toLowerCase() !== 'admin' || !isPasswordValid)
-    //   return res.status(400).json({ error: 'Usuario o contraseña incorrecto' })
-    if (username !== 'admin' || password !== 'admin')
-      return res.status(400).json({ error: 'Usuario o contraseña incorrecto' })
-
-    setCookie('user', JSON.stringify({ username, password, role: 'ADMIN' }), {
-      req,
-      res,
-      maxAge: 60 * 60 * 24,
+    return new Promise((resolve, reject) => {
+      db.query(
+        'SELECT * FROM usuarios WHERE usuario = ? AND clave = ?',
+        [username, password],
+        (err, data) => {
+          if (err) {
+            console.log('error', error)
+            res.status(500).json({ error: 'Error interno del servidor' })
+            return resolve()
+          }
+          if (data.length === 0) {
+            res.status(400).json({ error: 'Usuario o contraseña incorrecto' })
+          }
+          console.log('data', data)
+          setCookie(
+            'user',
+            JSON.stringify({ username, password, role: 'ADMIN' }),
+            {
+              req,
+              res,
+              maxAge: 60 * 60 * 24,
+            }
+          )
+          res.status(201).json({ username, password, role: 'ADMIN' })
+          return resolve()
+        }
+      )
     })
-    // setCookie('user', JSON.stringify(admin), { req, res, maxAge: 60 * 60 * 24 })
-    // res.status(201).json(admin)
-    res.status(201).json({ username, password, role: 'ADMIN' })
   }
 }
