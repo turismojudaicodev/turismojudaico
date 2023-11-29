321df2936eff55d46eec36504a0fe4ae9bdea400
Lucas <lucaspiputto@gmail.com>
2023-11-29 11:31:02 -0300

    Fetch correct number of tours per country

--- src/pages/api/content/tours/countries.js
+++ src/pages/api/content/tours/countries.js
@@ -6,13 +6,14 @@ export default async function handler(req, res) {
     let queryParams = []
 
     if (params.estado) {
-      queryParams.push(`p.estado=${params.estado}`)
+      queryParams.push(`pq.estado=${params.estado}`)
     }
 
     const queryString = `SELECT p.codigo, p.nombre, p.nombre_en, COUNT(*) as tours 
     FROM ciudades_x_paquete cxp 
     INNER JOIN ciudades c ON c.codigo = cxp.ciudad 
-    INNER JOIN paises p ON p.codigo = c.pais ${
+    INNER JOIN paises p ON p.codigo = c.pais 
+    INNER JOIN paquetes pq ON pq.codigo = cxp.paquete ${
       queryParams.length > 0 ? 'WHERE ' + queryParams.join(' AND ') : ''
     } GROUP BY p.codigo`
 
