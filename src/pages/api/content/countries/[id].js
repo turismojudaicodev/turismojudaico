95cf0d95d1426a662c2b5f8cbbb3fcb8dbb122fb
Lucas <lucaspiputto@gmail.com>
2023-11-30 18:36:38 -0300

    Enable country edition

--- src/pages/api/content/countries/[id].js
+++ src/pages/api/content/countries/[id].js
@@ -41,28 +41,41 @@ export default async function handler(req, res) {
     const { body } = req
     const entries = Object.entries(body)
 
-    console.log(entries)
+    let colsToUpdate = []
+    const colsValues = []
 
-    // return new Promise((resolve, reject) => {
-    //   db.query(
-    //     `UPDATE paises SET ... WHERE codigo=${countryId}`,
-    //     (err, data) => {
-    //       if (err) {
-    //         res
-    //           .status(500)
-    //           .json({ error: err.sqlMessage ?? 'Error al borrar país' })
-    //         return resolve()
-    //       }
-    //       res
-    //         .status(200)
-    //         .json({
-    //           message: `País con id "${countryId}" borrado exitosamente`,
-    //         })
-    //       return resolve()
-    //     }
-    //   )
-    // })
-    return res.status(200).json({ message: 'ok' })
+    entries.forEach(([key, value], i) => {
+      if (
+        value === '' ||
+        value === undefined ||
+        value === null ||
+        (typeof value === 'object' && Object.keys(value).length === 0)
+      )
+        return // Para evitar setear en string vacio imagenes no cambiadas
+      colsToUpdate.push(`${key}=?`)
+      colsValues.push(value)
+    })
+
+    const queryString = `UPDATE paises SET ${colsToUpdate.join(
+      ', '
+    )} WHERE codigo=${countryId}`
+
+    console.log(queryString)
+
+    return new Promise((resolve, reject) => {
+      db.query(queryString, colsValues, (err, data) => {
+        if (err) {
+          res
+            .status(500)
+            .json({ error: err.sqlMessage ?? 'Error al actualizar pais' })
+          return resolve()
+        }
+        res.status(200).json({
+          message: `Pais con id "${countryId}" actualizado exitosamente`,
+        })
+        return resolve()
+      })
+    })
   } else {
     return res.status(405).json({ error: 'Method not allowed' })
   }
