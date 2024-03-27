cd1c87d204eb87640051391270d98fe0ddd24faa
Lucas <lucaspiputto@gmail.com>
2024-03-27 10:53:25 -0300

    Latest requested changes

--- src/pages/api/content/posts/table.js
+++ src/pages/api/content/posts/table.js
@@ -39,7 +39,10 @@ export default async function handler(req, res) {
           }
           return acc
         }, {})
-        res.status(200).json({ data: Object.values(posts) })
+        const postsOrderedByOrder = Object.values(posts).sort(
+          (a, b) => a.orden - b.orden
+        )
+        res.status(200).json({ data: postsOrderedByOrder })
         return resolve()
       })
     })
